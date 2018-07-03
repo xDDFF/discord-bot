c1f1717d02d076d62fca6a7e1ab21dfe4335a316
xDDFF <something.very.random.here@gmail.com>
2018-07-03 23:02:20 +0000

    second commit

--- bartender.py
+++ bartender.py
@@ -1,14 +1,30 @@
-import discord
-import time
-from discord.ext import commands
+import os
+import datetime
+import sys
+from subprocess import *
+try:
+	import discord
+	from discord.ext import commands
+except ImportError:
+	print('Discord.py is not installed!')
+	sys.exit(0)
+
 
 TOKEN = 'BOT_TOKEN'
 BOT_PREFIX = ('!', '.')
 
 bot = commands.Bot(command_prefix=BOT_PREFIX)
 
+def check_bot_dirs():
+	dirs = ("cogs", "data", "log")
+	for dir in dirs:
+		if not os.path.exists(dir):
+			print('[%s] directory not FOUND! creating...' % (dir))
+			os.makedirs(dir)
+
 @bot.event
 async def on_ready():
+	print('\n=-=-=-=-=-=-=-=-=-=-=-')
 	print('__bot_ready__')
 	print('BOT_USERNAME : ' + bot.user.name)
 	print('BOT_USER_ID : ' + str(bot.user.id))
@@ -19,14 +35,16 @@ async def on_ready():
 #async def on_command_error(ctx, error):
 
 @bot.command()
-async def greet(ctx):
-	await ctx.send('hello')
+async def beer(ctx):
+	nick = ctx.author.display_name
+	await ctx.send('_gives ' + nick + ' a cold beer!_')
+	await ctx.send('is a Warsteiner! ' + nick + ' enjoy!!')
 
 @bot.command()
-async def pack(ctx):
-	await ctx.send('Grab your weed! Grind your weed! get ready! this is going to be a channel wide tokeout in 1 minute!')
-	for x in reversed(range(5)):
-		await ctx.send('0x000%d' % (x))
-	await ctx.send('READY! FIRE UP YOUR BOWLS!!')
+async def joint(ctx):
+	nick = ctx.author.display_name
+	await ctx.send('_lights ' + nick + ' joint!!_')
+	await ctx.send('blaze brah!!!')
 
+check_bot_dirs()
 bot.run(TOKEN) 
---
+++ log/utils.py
@@ -0,0 +1,22 @@
+import time
+import os
+from subprocess import call
+
+t = time.localtime(time.time())
+PATH = '/home/user/discord-bot/log/git.log'
+DATE = ('[' + str(t.tm_mon) + '-' + str(t.tm_mday) + '-' + str(t.tm_year) + ']')
+TIME = ('[' + str(t.tm_hour) + ':' + str(t.tm_min) + ']')
+
+def check_log_file():
+	if(os.path.exists(PATH) == False):
+        	print('LOG file does not exists....')
+        	print('Creating file [%s]' % (PATH))
+        	f = open(PATH, 'w+')
+        	f.write('############ git.log #############\n')
+        	f.close()
+	
+def write_log(arg):
+	f = open(PATH, 'a+')
+	f.write(DATE + ' ' + TIME + '-- commit submitted] ' + '[' + arg + ']\n')
+	f.close()
+
